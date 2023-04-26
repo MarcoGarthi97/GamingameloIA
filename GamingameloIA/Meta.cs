1829c2e8a0ebf8f884c146c925d7498049463580
MarcoGarthi97 <marcogarthi@gmail.com>
2023-04-26 23:50:13 +0200

    Insert post to instagram

--- GamingameloIA/Meta.cs
+++ GamingameloIA/Meta.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace GamingameloIA
 {
     internal class Meta
     {
+        private string _link = "https://graph.facebook.com/v16.0/";
+        private string _token = File.ReadAllLines(Properties.Settings.Default.MetaToken)[0];
+        private string _profileID = File.ReadAllLines(Properties.Settings.Default.ProfileID)[0];
+
+        public string CreateContainer(string link, string caption)
+        {
+            try 
+            {
+                var client = new RestClient();
+                var request = new RestRequest(_link + _profileID + "/media?image_url=" + link + "&resize=1200%3A*&caption=" + caption + "&access_token=" + _token, Method.Post);
+                RestResponse response = client.Execute(request);
+                Console.WriteLine(response.Content);
+
+                var val = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
+
+                return val.First().Value;
+            }
+            catch(Exception ex)
+            {
+                return "";
+            }
+        }
+
+        public string PublicContainer(string id)
+        {
+            try
+            {
+                var client = new RestClient();
+                var request = new RestRequest(_link + _profileID + "/media_publish?creation_id=" + id + "&access_token=" + _token, Method.Post);
+                RestResponse response = client.Execute(request);
+                Console.WriteLine(response.Content);
+
+                return response.Content;
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
     }
 }
--- GamingameloIA/Program.cs
+++ GamingameloIA/Program.cs
@@ -10,6 +10,9 @@ namespace GamingameloIA
     {
         static void Main(string[] args)
         {
+            Meta meta = new Meta();
+            string id = meta.CreateContainer("https%3A%2F%2Fhips.hearstapps.com%2Fhmg-prod%2Fimages%2Fhow-to-keep-ducks-call-ducks-1615457181.jpg%3Fcrop%3D0.669xw%3A1.00xh%3B0.166xw%2C0", "My mum <3");
+            string insert = meta.PublicContainer(id);
         }
     }
 }
