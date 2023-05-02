9b655ca90d5450fe792ba2ee024f9d98a8798e01
MarcoGarthi97 <marcogarthi@gmail.com>
2023-05-03 00:40:45 +0200

    create a site reader for get news

--- GamingameloIA/Program.cs
+++ GamingameloIA/Program.cs
@@ -10,9 +10,12 @@ namespace GamingameloIA
     {
         static void Main(string[] args)
         {
-            Meta meta = new Meta();
-            string id = meta.CreateContainer("https%3A%2F%2Fhips.hearstapps.com%2Fhmg-prod%2Fimages%2Fhow-to-keep-ducks-call-ducks-1615457181.jpg%3Fcrop%3D0.669xw%3A1.00xh%3B0.166xw%2C0", "My mum <3");
-            string insert = meta.PublicContainer(id);
+            //Meta meta = new Meta();
+            //string id = meta.CreateContainer("https://www.lapaginaonline.it/giornale/wp-content/uploads/2018/12/Attachment-1.jpeg", "My mum <3");
+            //string insert = meta.PublicContainer(id);
+
+            WebSite site = new WebSite();
+            site.GetInformationsSite("https://www.gamesradar.com/news/games/");
         }
     }
 }
---
+++ GamingameloIA/WebSite.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using static System.Collections.Specialized.BitVector32;
+
+namespace GamingameloIA
+{
+    internal class WebSite
+    {
+        public void GetInformationsSite(string url)
+        {
+            var lastThree = GetLatestNews(url);
+            if(lastThree.Count < 3)
+            {
+                //Error Code..
+            }
+
+            foreach(string link in lastThree)
+            {
+                var response = GetStructureSite(link);
+
+                int start = response.IndexOf("<section class=\"content-wrapper\"");
+                int end = response.IndexOf("</section>", start);
+                string articleHtml = response.Substring(start, end - start);
+
+                string article = "";
+                string startFilter = "<p>";
+                string endFilter = "</p>";
+                while (true)
+                {
+                    if (!articleHtml.Contains(startFilter))
+                        break;
+
+                    int startIndex = articleHtml.IndexOf(startFilter) + startFilter.Length;
+                    int endIndex = articleHtml.IndexOf(endFilter, startIndex);
+                    article += articleHtml.Substring(startIndex, endIndex - startIndex);
+
+                    articleHtml = articleHtml.Remove(startIndex - startFilter.Length, endIndex - startIndex + endFilter.Length);
+                }
+
+                article = CleanUpArticle(article);
+            }
+        }
+
+        private string CleanUpArticle(string article)
+        {
+            string startFilter = "<a";
+            string endFilter = "</a>";
+
+            while (true)
+            {
+                if (!article.Contains(startFilter))
+                    break;
+
+                int startIndex = article.IndexOf(startFilter);
+                int endIndex = article.IndexOf(endFilter, startIndex);
+                string a = article.Substring(startIndex, endIndex - startIndex + endFilter.Length);
+
+                int startA = article.IndexOf("<u>") + "<u>".Length;
+                int endA = article.IndexOf("</u>", startIndex);
+                string val = article.Substring(startA, endA - startA);
+
+                article = article.Replace(a, val);
+
+                if (article.Contains("<span"))
+                {
+                    int startSpan = article.IndexOf("<span");
+                    int endSpan = article.IndexOf("</span>", startSpan);
+
+                    article = article.Remove(startSpan, endSpan - startSpan + 7);
+
+                    article = article.Replace("<em>", "").Replace("</em>", "");
+                }
+            }
+
+            return article;
+        }
+        private List<string> GetLatestNews(string url)
+        {
+            var response = GetStructureSite(url);
+            string section = response.Substring(response.IndexOf("<section data-next=\"latest\""), response.IndexOf("<div class=\"static-lightbox3 ad-unit\">") - response.IndexOf("<section data-next=\"latest\""));
+
+            List<string> lastThree = new List<string>();
+            for (int i = 0; i < 3; i++)
+            {
+                int startIndex = section.IndexOf("href=\"") + 6;
+                int endIndex = section.IndexOf("\"", startIndex + 1);
+                string link = section.Substring(startIndex, endIndex - startIndex);
+
+                lastThree.Add(link);
+
+                section = section.Remove(startIndex - 7, endIndex - startIndex + 7);
+            }
+
+            return lastThree;
+        }
+
+        private string GetStructureSite(string url)
+        {
+            using (var webClient = new WebClient())
+            {
+                var response = webClient.DownloadString(url);
+                Console.WriteLine(response);
+
+                return response;
+            }
+        }
+    }
+}
